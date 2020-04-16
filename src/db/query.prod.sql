3a7c5a0c0626ef83bed230d51dad26275b627424
gaohan1994 <871418277@qq.com>
2020-04-16 19:04:37 +0800

    * version 1.0.0

--- src/db/query.prod.sql
+++ src/db/query.prod.sql
@@ -1,8 +1,14 @@
 
-INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','测试1');
-INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','测试2');
-INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','测试3');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','二手图书');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','美容美妆');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','手机数码');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','服饰鞋包');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','微易租房');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','小吃零食');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','日用百货');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'0','自定义');
 
-INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'1','测试1');
-INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'1','测试2');
-INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'1','测试3');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'1','学习经验');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'1','校园八卦');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'1','周边美食');
+INSERT INTO `market_type` (`id`,`type`,`name`) VALUES (DEFAULT,'1','热门兼职');
